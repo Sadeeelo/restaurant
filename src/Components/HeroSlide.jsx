4dcbde01b08a3239f28baa29c81259226adc63c7
aaron betts <aaronrbetts@gmail.com>
2024-05-21 12:55:10 +0300

    updated for 2024 season

--- src/Components/HeroSlide.jsx
+++ src/Components/HeroSlide.jsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import cafe from '../Images/cafe.jpg'
-import cafe2 from '../Images/Cafe2.jpg'
-import cafe3 from '../Images/Cafe3.jpg'
 import { Fade } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css';
 
@@ -28,9 +25,9 @@ const HeroSlide = () => {
             <div className='HeroSlide'>
             <img class="overlay" src="http://www.psd-dude.com/tutorials/images/animated-sunlight-by-psddude.gif" alt="Animated Sunlight by PSDDude"></img>
                 <Fade cssClass={'heroSlider'} arrows={false} slidesToScroll={3} slidesToShow={3} transitionDuration={2000} duration={2000} indicators={true} responsive={responsiveSettings}>
-                    <div><img src={cafe} className="heroSlideImage" alt="cafe" srcset="" /></div>
-                    <div><img src={cafe2} className="heroSlideImage" alt="cafe" srcset="" /></div>
-                    <div><img src={cafe3} className="heroSlideImage" alt="cafe" srcset="" /></div>
+                    <div><img src={'https://i.imgur.com/NZRLMkp.jpeg'} className="heroSlideImage" alt="cafe" srcset="" /></div>
+                    <div><img src={'https://i.imgur.com/hdxyeZr.jpeg'} className="heroSlideImage" alt="cafe" srcset="" /></div>
+                    <div><img src={'https://i.imgur.com/gHyr0oA.jpeg'} className="heroSlideImage" alt="cafe" srcset="" /></div>
                 </Fade>
             </div>
 
