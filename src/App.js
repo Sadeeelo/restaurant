43c8c764fbc22ea3d68b4e469a52ab56c7ec83f0
Sadeeelo <102422166+Sadeeelo@users.noreply.github.com>
2025-05-24 13:40:31 +0300

    palautus alkuperäiseen versioon

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Landing from './Pages/Landing';
-import Rent from './Components/Rent'; // <-- lisää tämä
+import { BrowserRouter, Route, Link, Routes } from 'react-router-dom';
+import Landing from './Pages/Landing'
 
 import './App.css';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/vuokraa" element={<Rent />} />
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <Landing />
+    </div>
   );
 };
 
