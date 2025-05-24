855b263650db7a563598d9dad03534dc621e1eda
Sadeeelo <102422166+Sadeeelo@users.noreply.github.com>
2025-05-24 15:03:17 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import { BrowserRouter, Route, Link, Routes } from 'react-router-dom';
-import Landing from './Pages/Landing'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Landing from './Pages/Landing';
+import Rent from './Components/Rent';
+import Menu from './Components/aaaMenu'; // tämä rivi on uusi
 
 import './App.css';
 
 const App = () => {
   return (
-    <div>
-      <Landing />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/vuokraa" element={<Rent />} />
+        <Route path="/menu" element={<Menu />} /> {/* uusi menu-näkymä */}
+      </Routes>
+    </BrowserRouter>
   );
 };
 
